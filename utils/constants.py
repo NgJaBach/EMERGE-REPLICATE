5c82fdbe799f6ba1337ba8bc13a09dafc63ab2a4
NgJaBach <ngbach2008@email.com>
2025-09-23 12:26:50 +0700

    complete 100%

--- utils/constants.py
+++ utils/constants.py
@@ -5,21 +5,21 @@ def ensure_path(THE_PATH: str):
     return THE_PATH
 
 # PRIMEKG - dataverse_files
-DATAVERSE_DIR   = "D:/Lab/Research/EMERGE-REPLICATE/datasets/dataverse_files/"
+DATAVERSE_DIR   = "D:/Lab/Research/HERMES-EHR/datasets/dataverse_files/"
 PRIMEKG_KG      = DATAVERSE_DIR + "kg.csv"
 PRIMEKG_DISEASE = DATAVERSE_DIR + "disease_features.csv"
 
 # Setup
-LLM_LOG = ensure_path("D:/Lab/Research/EMERGE-REPLICATE/utils/log.txt")
-GOOD_DATA = "D:/Lab/Research/EMERGE-REPLICATE/good_data/"
+LLM_LOG = ensure_path("D:/Lab/Research/HERMES-EHR/utils/log.txt")
+GOOD_DATA = "D:/Lab/Research/HERMES-EHR/good_data/"
 DATA_PATH = GOOD_DATA + f"complete/"
 RECORDS      = 4
 RANDOM_STATE = 42 # for reproducibility
 USE_CHUNKING = False
 
 # PART 1
-NOTES_ZHU  = "D:/Lab/Research/EMERGE-REPLICATE/preprocessing-zhu/mimic-iii/data/processed/notes.csv"
-EHR_ZHU    = "D:/Lab/Research/EMERGE-REPLICATE/preprocessing-zhu/mimic-iii/data/processed/ehr.csv"
+NOTES_ZHU  = "D:/Lab/Research/HERMES-EHR/preprocessing-zhu/mimic-iii/data/processed/notes.csv"
+EHR_ZHU    = "D:/Lab/Research/HERMES-EHR/preprocessing-zhu/mimic-iii/data/processed/ehr.csv"
 NOTES_BACH = ensure_path(GOOD_DATA + "processed/notes.csv")
 EHR_BACH   = ensure_path(GOOD_DATA + "processed/ehr.csv")
 
